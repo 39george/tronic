aaeb63e1d0fbc9ebdc144b2537e9dc207f969318
39george <obsidian.musicwork@gmail.com>
2026-01-26 17:19:11 +0300

    fix: mock-provider

--- src/provider/mock.rs
+++ src/provider/mock.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use anyhow::anyhow;
+use eyre::eyre;
 
 use crate::Result;
 use crate::contracts::AbiEncode;
@@ -26,7 +26,7 @@ impl crate::provider::TronProvider for MockProvider {
         _: domain::address::TronAddress,
         _: trx::Trx,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn trigger_smart_contract<A: AbiEncode + Send>(
         &self,
@@ -34,94 +34,94 @@ impl crate::provider::TronProvider for MockProvider {
         _: TronAddress,
         _: A,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn broadcast_transaction(
         &self,
         _: domain::transaction::Transaction,
     ) -> Result<()> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn estimate_energy(
         &self,
         _: domain::contract::TriggerSmartContract,
     ) -> Result<i64> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_account(
         &self,
         _: TronAddress,
     ) -> Result<domain::account::Account> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_account_resources(
         &self,
         _: TronAddress,
     ) -> Result<domain::account::AccountResourceUsage> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn trigger_constant_contract(
         &self,
         _: domain::contract::TriggerSmartContract,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_now_block(&self) -> Result<domain::block::BlockExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn account_permission_update(
         &self,
         _: domain::contract::AccountPermissionUpdateContract,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_transaction_by_id(
         &self,
         _: Hash32,
     ) -> Result<domain::transaction::Transaction> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_transaction_info(
         &self,
         _: Hash32,
     ) -> Result<domain::transaction::TransactionInfo> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn chain_parameters(&self) -> Result<HashMap<String, i64>> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn freeze_balance(
         &self,
         _: domain::contract::FreezeBalanceV2Contract,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn unfreeze_balance(
         &self,
         _: domain::contract::UnfreezeBalanceV2Contract,
     ) -> Result<domain::transaction::TransactionExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_reward(&self, _address: TronAddress) -> Result<Trx> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_delegated_resource(
         &self,
         _: TronAddress,
         _: TronAddress,
     ) -> Result<Vec<domain::account::DelegatedResource>> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_delegated_resource_account(
         &self,
         _: TronAddress,
     ) -> Result<domain::account::DelegatedResourceAccountIndex> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
     async fn get_block_by_number(
         &self,
         _block_num: i64,
     ) -> Result<domain::block::BlockExtention> {
-        Err(Error::Unexpected(anyhow!("mock provider")))
+        Err(Error::Unexpected(eyre!("mock provider")))
     }
 }
